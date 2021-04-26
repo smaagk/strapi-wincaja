2943b68d1f0cb6b4b190946151179e976344a0cb
smaagk <smaa.gk@gmail.com>
2021-04-26 13:22:51 -0500

    inital commit

---
+++ cms/my-project/config/plugins.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => {
+ return ({
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: "us-west-2",
+      params: {
+        Bucket: env("BUCKET"),
+      },
+    },
+  },
+})
+};
---
+++ cms/my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '746662f1-5d37-42d5-a771-bc16e29d2bd1'
+};
